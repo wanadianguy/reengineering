957c8a5103beedfa384779ae50e92709a9b1c7b3
Wonkruu <williamdpe@gmail.com>
2023-11-10 17:30:11 +0100

    feat(delivecrous): docker

--- delivecrous/index.js
+++ delivecrous/index.js
@@ -3,11 +3,13 @@ const app = express();
 const mongoose = require("mongoose");
 const IndexRouter = require("./routers/index");
 
-mongoose.connect("mongodb://localhost:27017/delivecrous");
+mongoose.connect("mongodb://localhost:27017/delivecrous")
+    .then(() => console.log('Database connected'))
+    .catch(() => console.log('Database not found'));
 
 app.use(express.json());
 app.use("/api", IndexRouter);
 
 app.listen(process.env.PORT || "3000", () => {
-    console.log("Le serveur est à l’écoute sur le port 3000");
+    console.log("Server started on port 3000");
 });
