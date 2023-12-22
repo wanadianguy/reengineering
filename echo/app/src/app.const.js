f687caed08c33e5de6d03c4c5202754a7d5cdcca
bryan <bryan@DESKTOP-PO3Q5D1.local>
2023-12-22 14:30:23 +0100

    Test de récupération de la variable d'environnement

--- echo/app/src/app.const.js
+++ echo/app/src/app.const.js
@@ -1,1 +1,10 @@
-export let baseApiUrl = 'http://localhost:8080';
+require('dotenv').config()
+let url;
+console.log("env:",process.env)
+console.log("REQUEST_API_URL:",process.env.REQUEST_API_URL)
+if(process.env.REQUEST_API_URL){
+    url= process.env.REQUEST_API_URL;
+}else {
+    url = 'http://localhost:8080';
+}
+export const baseApiUrl = url;
